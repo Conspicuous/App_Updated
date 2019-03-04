9d31d433f45fca53a82792a49487337acd449652
ggw <geekgirlwisdom@gmail.com>
2019-03-03 22:04:26 -0500

    Merged CPD elements of App. Edited button menus on all screens to display correct buttons for presence of and lack of data. Added button to return to dashboard that gets around the issue of a refresh function (by returning to home view, the layout elements are refreshed. TODO: Calendar Date Element--once goal created, popup--when should this be completed by, user enters-->stored-->saved for goal progress. Weekly notifications.d

--- app/src/main/java/com/example/smartgoals/navigator_0/GoalProgressBarFragment.java
+++ app/src/main/java/com/example/smartgoals/navigator_0/GoalProgressBarFragment.java
@@ -5,20 +5,41 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class GoalProgressBarFragment extends Fragment {
 
-    private static final int TEST_PERCENT_COMPLETE = 68;
-
+    private ProgressBar progressBar;
     public GoalProgressBarFragment() {
+    }
+
+    public static GoalProgressBarFragment newInstance(int GoalProgressPercent) {
+
+        GoalProgressBarFragment G = new GoalProgressBarFragment();
+        Bundle args = new Bundle();
+        args.putInt("GoalProgressPercent", GoalProgressPercent);
+        G.setArguments(args);
+        return G;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 //     View view = inflater.inflate(R.layout.fragment_progress_bars, container, false);
-        View view = inflater.inflate(R.layout.fragment_progress_bars, container, false);
+        View view = inflater.inflate(R.layout.goal_progress_bar_fragment, container, false);
 
+        Bundle args = getArguments();
+        int PercentComplete = args.getInt("GoalProgressPercent", 0);
+        progressBar = view.findViewById(R.id.Goal_ProgressBar_PercentCompleted);
+        //progressBar.setProgress(TEST_PERCENT_COMPLETE);
+        progressBar.setProgress(PercentComplete);
+        String percentComplete = getString(R.string.Percent_Complete, PercentComplete);
+        String percent = getString(R.string.percent_sign);
+        String complete = getString(R.string.Complete);
+        TextView textView = view.findViewById(R.id.Percent_Complete_Text);
+        String conCat = percentComplete + percent + ' ' + complete;
+        textView.setText(conCat);
 //    TextView goalProgress = view.findViewById(R.id.Goal_Progress_Text);
 //    goalProgress.setText(R.string.Goal_Progress_Title);
 
@@ -27,6 +48,7 @@ public class GoalProgressBarFragment extends Fragment {
 }
 
 
+//TODO: Poll the data entry
 
 
 
